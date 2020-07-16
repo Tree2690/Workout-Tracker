db9045ecdcf55ef202fe56083338f08728c445a2
Jonathan Liang <jonathanliang2690123@gmail.com>
2020-07-15 22:13:47 -0700

    modifited config to fit the frontend

--- config/index.js
+++ config/index.js
@@ -1,1 +1,2 @@
-module.exports = require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI)
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI,
+    {useNewUrlParser: true, useFindAndModify: false })
